8c662894af82a4fdf669a0187f9d75f18c8b45fb
weihaishan <2579488955@qq.com>
2020-09-27 19:47:38 +0800

    添加个人中心模块

---
+++ UserCenter/src/main/java/com/oscar/usercenter/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.oscar.usercenter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oscar.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
